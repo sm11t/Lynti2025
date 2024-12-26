2e22d068ce5c830f4549166235905d433dd0604d
sm11t <adatta18@asu.edu>
2024-12-25 17:56:09 -0700

    progress 3
    implemented the base for the onboarding cards with custom text. need to change the images and customize according to your needs.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function SignUp() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-red-200">
-      <Text className="text-lg text-blue-600 font-bold">Sign ip</Text>
+      <Text className="text-lg text-blue-600 font-bold">Sign up</Text>
     </SafeAreaView>
   );
 }
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,50 @@
-import { View, Text } from "react-native";
+import { Text, TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
 export default function Onboarding() {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-red-200">
-      <Text className="text-lg text-blue-600 font-bold">Onboarding</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View
+            key={item.id}
+            className="flex-1 items-center justify-center p-5"
+          >
+            <Image
+              source={item.image}
+              className="h-[300px] w-full object-cover"
+              resizeMode="contain"
+            />
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 }
