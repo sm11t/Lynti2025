57ed7dda58d8d550a32140db2188ae209047629a
sm11t <adatta18@asu.edu>
2025-01-04 22:33:13 -0700

    Greeting + Search Bar
    Needs improvement on placement and needs a top left slide to the left bar

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,5 +1,6 @@
 import {SignedIn, SignedOut, useUser} from "@clerk/clerk-expo";
-import {SafeAreaView, Text} from "react-native";
+import {SafeAreaView, Text, TextInput, View} from "react-native";
+import {FontAwesome} from "@expo/vector-icons";
 
 export default function HomeScreen() {
     const {user} = useUser();
@@ -8,16 +9,32 @@ export default function HomeScreen() {
         <SafeAreaView className="flex-1 bg-razer-black px-5">
             {/* Greeting Section */}
             <SignedIn>
-                <Text className="text-razer-green text-2xl font-JakartaSemiBold mt-10">
+                <Text className="text-razer-green text-2xl font-JakartaSemiBold mt-16 pl-4">
                     Hello {user?.firstName}, Welcome to Lynti!
                 </Text>
             </SignedIn>
 
             <SignedOut>
-                <Text className="text-razer-green text-2xl font-JakartaSemiBold mt-10">
+                <Text className="text-razer-green text-2xl font-JakartaSemiBold mt-16 pl-4">
                     Welcome to Lynti! Please sign in to continue.
                 </Text>
             </SignedOut>
+
+            {/* Search Bar Section */}
+            <View
+                className="flex-row items-center mx-auto mt-6 border border-razer-green bg-razer-darkGray rounded-full h-16 w-11/12 px-4 shadow-lg"
+            >
+                <FontAwesome name="search" size={20} color="#A6A6A6"/>
+                <TextInput
+                    placeholder="Where are you going?"
+                    placeholderTextColor="#A6A6A6"
+                    style={{
+                        fontSize: 18,
+                        fontFamily: "Inter_400Regular",
+                    }} // Set font size here
+                    className="flex-1 text-white ml-3 text-left"
+                />
+            </View>
         </SafeAreaView>
     );
 }
